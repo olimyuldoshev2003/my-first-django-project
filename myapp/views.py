4bd0b2fb3d95e1a04d6d7397a79652d7fe43358a
Olim Yuldoshev <oyuldoshev39@gmail.com>
2024-07-02 13:33:04 +0500

    Created the counter

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+    # aboutMe = {"name": "Olim", "surname": "Yuldoshev"}
+    # return render(request, "index.html", aboutMe)
+    return render(request, "index.html")
+
+
+# def counter(request):
+#     text = request.GET["text"]
+#     lengthOfText = len(text.split())
+
+#     # dictionaryText = {"text": text}
+#     dictionaryLengthOfText = {"lengthOfText": lengthOfText}
+
+
+#     # return render(request, "counter.html", dictionaryText)
+#     return render(request, "counter.html", dictionaryLengthOfText)
+def counter(request):
+    text = request.POST["text"]
+    lengthOfText = len(text.split())
+
+    # dictionaryText = {"text": text}
+    dictionaryLengthOfText = {"lengthOfText": lengthOfText}
+
+    # return render(request, "counter.html", dictionaryText)
+    return render(request, "counter.html", dictionaryLengthOfText)
